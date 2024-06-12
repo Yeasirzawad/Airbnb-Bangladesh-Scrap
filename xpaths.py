91abc47cf6eefb45afff283bc361816e88991e99
Ayan <islamayan123456@gmail.com>
2024-06-13 01:28:50 +0600

    apartment listing fetch

---
+++ xpaths.py
@@ -0,0 +1,5 @@
+xpaths_dict= {
+    "result_count_xpath":"//span[@class='tyi4kqb atm_c8_km0zk7 atm_g3_18khvle atm_fr_1m9t47k atm_cs_9dzvea dir dir-ltr']",
+    "apartment_link_xpath":"(//div[@class='cy5jw6o atm_5j_223wjw atm_70_87waog atm_j3_1u6x1zy atm_jb_4shrsx atm_mk_h2mmj6 atm_vy_7abht0 dir dir-ltr']/a)",
+    "next_page_xpath":"//div[@class='p1j2gy66 atm_9s_1txwivl dir dir-ltr']/*[last()]"
+}
