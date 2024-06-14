968ecbc2277d7f24c16d44e57a2e965275daab85
Ayan <islamayan123456@gmail.com>
2024-06-14 14:14:29 +0600

    data fetched

--- xpaths.py
+++ xpaths.py
@@ -12,15 +12,17 @@ xpaths_dict= {
    
     "review_count_xpath":"//div[@class='rk4wssy atm_c8_km0zk7 atm_g3_18khvle atm_fr_1m9t47k atm_cs_9dzvea atm_h3_ftgil2 atm_9s_1txwivl atm_h_1h6ojuz atm_cx_1y44olf atm_c8_2x1prs__oggzyc atm_g3_1jbyh58__oggzyc atm_fr_11a07z3__oggzyc dir dir-ltr']/a",
     "ind_rev_xpath": "//div[@class='vjb6p42 atm_c8_vvn7el atm_g3_k2d186 atm_fr_1vi102y dir dir-ltr']",
-    "price_xpath":"//span[@class = '_1y74zjx']"
->>>>>>> 90f98dc9fa99421d5e1227178384df5215f92d5d
+  
+
     "host_link_Xpath": "//div[@class='colzjmk atm_mk_stnw88 atm_tk_idpfg4 atm_6i_idpfg4 atm_fq_idpfg4 atm_n3_idpfg4 dir dir-ltr']/a",
-    "response_rate_Xpath": "//div[@class='h1geptgj atm_9s_1txwivl atm_ar_1bp4okc atm_cx_exct8b dir dir-ltr']/div[1]",
-    "facilities_xpath": "//div[@class='twad414 atm_7l_jt7fhx atm_9j_1kw7nm4 atm_bx_48h72j atm_c8_2x1prs atm_g3_1jbyh58 atm_fr_11a07z3 dir dir-ltr']"
+    "response_rate_Xpath": "//div[@class='h1geptgj atm_9s_1txwivl atm_ar_1bp4okc atm_cx_exct8b dir dir-ltr']/*[2]/*[1]",
+    "facilities_xpath": "//div[@class='twad414 atm_7l_jt7fhx atm_9j_1kw7nm4 atm_bx_48h72j atm_c8_2x1prs atm_g3_1jbyh58 atm_fr_11a07z3 dir dir-ltr']",
     "facilities_button_xpath":"//button[@class='l1ovpqvx atm_1he2i46_1k8pnbi_10saat9 atm_yxpdqi_1pv6nv4_10saat9 atm_1a0hdzc_w1h1e8_10saat9 atm_2bu6ew_929bqk_10saat9 atm_12oyo1u_73u7pn_10saat9 atm_fiaz40_1etamxe_10saat9 b65jmrv atm_9j_tlke0l atm_9s_1o8liyq atm_gi_idpfg4 atm_mk_h2mmj6 atm_r3_1h6ojuz atm_rd_glywfm atm_3f_uuagnh atm_70_5j5alw atm_vy_1wugsn5 atm_tl_1gw4zv3 atm_9j_13gfvf7_1o5j5ji v7aged4 atm_bx_48h72j atm_c8_2x1prs atm_g3_1jbyh58 atm_cs_9dzvea atm_5j_t09oo2 atm_6h_t94yts atm_66_nqa18y atm_uc_1c5iv21 atm_kd_glywfm atm_4b_dezgoh atm_26_1p8m8iw atm_7l_dezgoh atm_l8_1vkzbvs atm_uc_glywfm__1rrf6b5 atm_3f_glywfm_jo46a5 atm_l8_idpfg4_jo46a5 atm_gi_idpfg4_jo46a5 atm_3f_glywfm_1icshfk atm_kd_glywfm_19774hq atm_uc_aaiy6o_1w3cfyq atm_uc_glywfm_1w3cfyq_1rrf6b5 atm_uc_aaiy6o_pfnrn2_1oszvuo atm_uc_glywfm_pfnrn2_1o31aam atm_tr_18md41p_csw3t1 atm_k4_kb7nvz_1o5j5ji atm_4b_dezgoh_1w3cfyq atm_7l_dezgoh_1w3cfyq atm_70_1x1xnix_1w3cfyq atm_4b_dezgoh_pfnrn2_1oszvuo atm_7l_dezgoh_pfnrn2_1oszvuo atm_70_1x1xnix_pfnrn2_1oszvuo atm_4b_1cpjb8p_1nos8r atm_26_116dmco_1nos8r atm_7l_dezgoh_1nos8r atm_4b_1cpjb8p_csw3t1 atm_26_116dmco_csw3t1 atm_7l_dezgoh_csw3t1 atm_4b_1oqmvsg_1o5j5ji atm_26_1p8m8iw_1o5j5ji atm_7l_1oqmvsg_1o5j5ji dir dir-ltr']",
     "description_show_all_Xpath":"//div[@class='sx1pl6d atm_h3_8tjzot dir dir-ltr']/button",
-    "description_xpath":"//div[@data-section-id='DESCRIPTION_MODAL']/section)",
-    "description_close_Xpath":"//button[@aria-label='Close']"
-    "price_xpath":"(//span[@class = '_1y74zjx'])[1]"
+    "description1_xpath":"//div[@data-section-id='DESCRIPTION_MODAL']/section)",
+    "description_close_Xpath":"//button[@aria-label='Close']",
+    "description2_xpath":"//div[contains(@class, 'd1isfkwk') and contains(@class, 'atm_vv_1jtmq4') and contains(@class, 'atm_w4_1hnarqo') and contains(@class, 'dir') and contains(@class,'dir-ltr')]//span[not(.//span)]",
+    "price_xpath":"(//span[@class = '_1y74zjx'])[1]",
+    "button_close_div_path":"//div[@class='c11vnb9k atm_mk_stnw88 atm_9s_1txwivl atm_tk_exct8b atm_fq_1tcgj5g atm_wq_kb7nvz atm_1wn1q82_xond3e atm_tk_1tcgj5g__oggzyc dir dir-ltr']",
 
 }
